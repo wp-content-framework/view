c010768dd5894eb179e1c58d766c7d197ee0e9f1
technote <technote.space@gmail.com>
2019-02-12 23:29:18 +0900

    v0.0.3

--- src/views/include/form/color.php
+++ src/views/include/form/color.php
@@ -2,7 +2,7 @@
 /**
  * WP_Framework_View Views Include Form Color
  *
- * @version 0.0.1
+ * @version 0.0.3
  * @author technote-space
  * @copyright technote-space All Rights Reserved
  * @license http://www.opensource.org/licenses/gpl-2.0.php GNU General Public License, version 2
