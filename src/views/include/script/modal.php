96e93d68b051c3d46edd46f7c5ed559ed61c1663
technote <technote.space@gmail.com>
2019-03-06 23:11:43 +0900

    v0.0.6

--- src/views/include/script/modal.php
+++ src/views/include/script/modal.php
@@ -2,7 +2,7 @@
 /**
  * WP_Framework_View Views Include Script Modal
  *
- * @version 0.0.3
+ * @version 0.0.6
  * @author Technote
  * @copyright Technote All Rights Reserved
  * @license http://www.opensource.org/licenses/gpl-2.0.php GNU General Public License, version 2
