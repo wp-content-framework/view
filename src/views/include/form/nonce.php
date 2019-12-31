7b609047897eed8b5a68e168dbbc24109c52ad6f
Technote <technote.space@gmail.com>
2020-01-01 03:57:40 +0900

    chore: remove version info

--- src/views/include/form/nonce.php
+++ src/views/include/form/nonce.php
@@ -2,7 +2,6 @@
 /**
  * WP_Framework_View Views Include Form Nonce
  *
- * @version 0.0.8
  * @author Technote
  * @copyright Technote All Rights Reserved
  * @license http://www.opensource.org/licenses/gpl-2.0.php GNU General Public License, version 2
